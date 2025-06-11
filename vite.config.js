a95023a39e0be13ec45354c52935678aee8c0fdc
meistermode <overlorddd605@gmail.com>
2025-06-11 19:53:49 +0500

    -8000

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://https://meistermode-carsharing-backend-7481.twc1.net:8000',
-      '/storage': 'https://https://meistermode-carsharing-backend-7481.twc1.net:8000'
+      '/api': 'https://https://meistermode-carsharing-backend-7481.twc1.net',
+      '/storage': 'https://https://meistermode-carsharing-backend-7481.twc1.net'
     }
   }
 });
